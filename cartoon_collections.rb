c648be2c837eda742e099021cd462b55e1570a2d
David Gilbert <dagilbert333@gmail.com>
2018-12-28 09:20:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |item, index| 
+    puts "#{index + 1}. #{item}"
+  }
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |element|
+  element.capitalize + "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+ddef long_planeteer_calls(array)
+  
+  array.map! { |call| 
+    if call.length > 4
+      true 
+    else
+      false
+    end
+  }
+
+  if array.include?(true)
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
