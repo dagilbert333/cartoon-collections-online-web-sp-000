3b036e82ea8dbc85c0a3091d4f7f7cd8aa8440f9
David Gilbert <dagilbert333@gmail.com>
2018-12-28 09:46:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
   while i < array.length
-    yield(cheese_types[i])
+    return cheese_types[i] if yield(collection[i])
     i = i + 1
   end
 end
